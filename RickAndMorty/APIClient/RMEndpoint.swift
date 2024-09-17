c6a42c07a6f7d51faa14f27d3808054a75c44d20
Vaibhav <vaibhavchoudhary@Vaibhavs-MacBook-Air-2.local>
2024-09-17 23:27:18 +0530

    created file structure for api calls and some commenting

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,19 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Vaibhav on 17/09/24.
+//
+
+import Foundation
+
+/// Represents unique API endpoint
+@frozen enum RMEndpoint: String {
+    
+    ///Endpoint to get character info
+    case character
+    ///Endpoint to get location info
+    case location
+    ///Endpoint to get episode info
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Vaibhav on 17/09/24.
+//
+
+import Foundation
+
+/// Object that represents a single API Call
+final class RMRequest {
+    
+}
