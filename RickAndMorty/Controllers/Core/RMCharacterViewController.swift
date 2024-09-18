0454d58068fbe55f34af1d192302a5c8a09dca90
Vaibhav <vaibhavchoudhary@Vaibhavs-MacBook-Air-2.local>
2024-09-18 16:06:36 +0530

    created CharacterListView and CharacterListViewViewModel + added collectionView and UIActivityIndicator

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -10,20 +10,25 @@ import UIKit
 /// Controller to show and search for characters
 final class RMCharacterViewController: UIViewController {
     
+    private let characterListView = CharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Characters"
         
-        RMService.shared.execute(.listCharactersRequest, expecting: RMGetAllCharactersResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print("Total: "+String(model.info.pages))
-                print("Page result count "+String(model.results.count))
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(characterListView)
+        
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
